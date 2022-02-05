ddcca0a3c6b283a792d5ebf5133cb2b90c8187f0
parasaraib <manikparasara@ymail.com>
2022-02-05 11:51:17 +0700

    add additional information to dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -20,6 +20,12 @@ const Dashboard = () => {
             <div className="row">
               <a href="https://github.com/ParasaraIB/forharapp" target="_blank" rel="noreferrer" style={{textDecoration: "none"}}>Source Code</a>
             </div>
+            <div className="row">
+              <a href="https://www.figma.com/file/QBU188jk08rVQRaWqGisbd/ForharApp?node-id=0%3A1" target="_blank" rel="noreferrer" style={{textDecoration: "none"}}>Figma Design</a>
+            </div>
+            <div className="row">
+              <a href="https://drive.google.com/file/d/1mdTFqPxjW1Bk4ngjUpUQQ6AUPSRUoz4X/view?usp=sharing" target="_blank" rel="noreferrer" style={{textDecoration: "none"}}>ERD</a>
+            </div>
           </div>
           <Footer />
         </div>
